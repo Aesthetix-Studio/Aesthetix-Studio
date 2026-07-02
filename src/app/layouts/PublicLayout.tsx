61d5ee3198dc402663c2d4b6fd9e2b1ecc455234
Mohd Abrar Ali Khan <77969630+mohdabraralikhan@users.noreply.github.com>
2026-07-02 14:35:12 +0530

    Fix scroll-to-top, move showcase sites to portfolio folder

--- src/app/layouts/PublicLayout.tsx
+++ src/app/layouts/PublicLayout.tsx
@@ -1,10 +1,12 @@
-import { Outlet, useLocation } from "react-router";
+import { Outlet } from "react-router";
 import { HPNav } from "../components/hp-nav";
 import { AXFooter } from "../components/ds-footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function PublicLayout() {
   return (
     <div className="min-h-screen flex flex-col bg-background text-foreground">
+      <ScrollToTop />
       <HPNav />
       <main className="flex-1">
         <Outlet />
